c9f486a25ab40387eea69c493cc83c7f715eee0d
CodRjt <arjitgenai@gmail.com>
2025-07-17 16:54:19 +0530

    added semantic search

--- main.py
+++ main.py
@@ -2,26 +2,52 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
-
+from rapidfuzz import fuzz,process
+from sentence_transformers import SentenceTransformer,util
+from torch import topk
 st.title("Find me Book")
 
-st.write("## Your Interest")
+st.write("## Readaa!!!")
 
 search=st.text_input("Enter a book or author you like")
 # Author_name=st.text_input("Enter an author you like")
 # genre_name=st.text_input("Enter a genre you like")
 
+# def fuzzy_finder(df:pd.DataFrame,query,columns):
+#     search_pool=df[columns].astype(str).agg(" ".join,axis=1)
+#     # st.write(search_pool.tolist())
+#     result=process.extract(query,search_pool.tolist(),limit=None)
+#     data=[df.iloc[match[2]] for match in result if match[1] > 50]
+#     return pd.DataFrame(data)
+
+model=SentenceTransformer('all-MiniLM-L6-v2')
+def semantic_finder(df:pd.DataFrame,query,columns):
+    search_pool=df[columns].astype(str).agg(" ".join,axis=1)
+    # st.write(search_pool.tolist())
+    search_pool_embeddings=model.encode(search_pool.tolist(),convert_to_tensor=True)
+    query_embedding=model.encode(query,convert_to_tensor=True)
+
+    cosine_scores=util.pytorch_cos_sim(query_embedding,search_pool_embeddings)
+    return topk(cosine_scores,k=8)
+
+    
 data=pd.read_csv("books.csv")
 if search:
-    filtered=data[data['original_title'].str.contains(search,case=False)| data['authors'].str.contains(search,case=False)]
-    st.write(f"found {len(filtered)} matching books")
-    for _,row in filtered.head(10).iterrows():
+    filtered=semantic_finder(data,search,["authors","title"])
+    # st.write(filtered)
+    st.write("Top 8 matching books")
+    # st.write(f"found {len(filtered)} matching books")
+    # for _,row in filtered.head(10).iterrows():
+    for idx,score in zip(filtered.indices[0],filtered.values[0]):
+        row=data.iloc[idx.item()]
         col1,col2=st.columns(2)
         with col1:
             st.image(row['image_url'])
         with col2:
             st.subheader(row['original_title'])
-            st.write(f"Author:{row['authors']}")
-            st.write(f"ratings:{row['average_rating']}")
+            st.write(f"Author:{row['authors']}      score:{score}")
+            st.write(f"Year:{row['original_publication_year']}")
+            st.write(f"Language: {row['language_code']}")
+            st.write(f"Ratings:{row['average_rating']}({row['ratings_count']}) rating")
         
 
